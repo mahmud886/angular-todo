1a7b8b086a1855ebae73c67769dfb42781ced702
mahmud886 <iqbal886mahmud@gmail.com>
2023-11-27 16:18:49 +0000

    todo functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { Todo } from './model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-todo';
+  // Initial Todo
+  get initialTodo() {
+    return {
+      title: '',
+      id: null,
+    };
+  }
+  todoList: Todo[] = [];
+  todo: Todo = this.initialTodo;
+
+  // Add todo
+  addTodo(): void {
+    if (this.todo.id) {
+      this.todoList = this.todoList.map((item) => {
+        if (item.id === this.todo.id) {
+          item.title = this.todo.title;
+        }
+        return item;
+      });
+    } else {
+      this.todo.id = Date.now();
+      this.todoList.push({ ...this.todo });
+    }
+    console.log(this.todoList);
+
+    this.todo = this.initialTodo;
+  }
+
+  // Edit Todo
+  editTodo(todo: Todo): void {
+    this.todo = { ...todo };
+  }
+
+  // Delete Todo
+  deleteTodo(id: number): void {
+    this.todoList = this.todoList.filter((todo) => todo.id != id);
+  }
 }
